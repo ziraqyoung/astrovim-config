39c9b23416f966af45e739c49667cdb49c1d5715
ziraq young <ziraqyoung@outlook.com>
2023-10-13 21:10:13 +0300

    Use default config for ruby_ls(ruby-lsp)

--- lsp/config/ruby_ls.lua
+++ lsp/config/ruby_ls.lua
@@ -1,20 +1,20 @@
-local nvim_lsp = require "lspconfig"
-
-local enabled_features = {
-  "documentHighlights",
-  "documentSymbols",
-  "foldingRanges",
-  "selectionRanges",
-  "semanticHighlighting",
-  "formatting",
-  "codeActions",
-}
-
-return {
-  filetypes = { "ruby", "rakefile" },
-  cmd = { "ruby-lsp" },
-  root_dir = nvim_lsp.util.root_pattern("Gemfile", ".git", "."),
-  init_options = {
-    enabledFeatures = enabled_features,
-  },
-}
+-- local nvim_lsp = require "lspconfig"
+--
+-- local enabled_features = {
+--   "documentHighlights",
+--   "documentSymbols",
+--   "foldingRanges",
+--   "selectionRanges",
+--   "semanticHighlighting",
+--   "formatting",
+--   "codeActions",
+-- }
+--
+-- return {
+--   filetypes = { "ruby", "rakefile" },
+--   cmd = { "ruby-lsp" },
+--   root_dir = nvim_lsp.util.root_pattern("Gemfile", ".git"),
+--   init_options = {
+--     enabledFeatures = enabled_features,
+--   },
+-- }
--- plugins/mason.lua
+++ plugins/mason.lua
@@ -19,7 +19,7 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        'ruby_lsp'
+        -- 'ruby_lsp'
         -- "prettier",
         -- "stylua",
       })
